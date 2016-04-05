75055860b070859f3b5137d2b797240e30aee481
jamtor <j.torpy@garvan.org.au>
2016-04-05 12:36:11 +1000

    deleted unecessary script files

--- scripts/4.quantify.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#sample="endo_1"
-sample="FBS1"
-#for file in `dx ls /projects/$sample/trimgalore/*_R1_001_1.fq.gz`;do 
-for file in `dx ls /projects/$sample/trimgalore/*_1.fq.gz`;do
-	echo $file
-	dx run workflow-star-2.4.0j-quant-rsem-1.2.18 --dest /projects/$sample -istage-BZKBXGQ0qb7fQB86001Gg21v.reads=/projects/$sample/trimgalore/$file -istage-BZKBXGQ0qb7fQB86001Gg21v.reads2=/projects/$sample/trimgalore/${file%_1.fq.gz}_2.fq.gz --yes;
-done;
