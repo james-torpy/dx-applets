27b23c30f338d99117578105ce3d3b42856dc629
jamtor <j.torpy@garvan.org.au>
2016-04-12 09:37:35 +1000

    add first test script circ_rna_draft.bash

---
+++ test_scripts/scripts/circ_rna_draft.bash
@@ -0,0 +1,161 @@
+#!/bin/bash
+
+###CircRNA###
+
+module load pethum/bowtie2/prebuilt/2.2.6
+module load gi/samtools/1.2
+
+big_numcores=16
+small_numcores=3
+
+projectname="dx-applets"
+samplename="test_scripts"
+genomename="hg38_ercc"
+
+#make directory hierachy:
+homeDir="/home/jamtor"
+projectDir="$homeDir/projects/$projectname/$samplename"
+resultsDir="$projectDir/results"
+genomeDir="/home/jamtor/genomes/$genomename"
+
+
+#find_circ location:
+circDir="/home/jamtor/local/bin/find_circ"
+
+#input/outputs:
+genomeFile="$genomeDir/$genomename.fa"
+inExt=".fq"
+outExt=".circ_rna"
+
+inDir="$projectDir/raw_files"
+
+#scripts/logs directory
+scriptsPath="$projectDir/scripts"
+logDir="$scriptsPath/logs"
+
+mkdir -p $logDir
+
+echo -e
+echo This is the genomeFile:
+echo $genomeFile
+echo -e
+echo This is the inDir:
+echo $inDir
+echo -e
+echo This is the logDir:
+echo $logDir
+
+#fetch file names of all projects and put into an array:
+i=0
+files=( $(ls $inDir/*$inExt))
+
+for file in ${files[@]} ;do
+	echo -e
+	echo The file used is: $file
+	filesTotal[i]=$file
+	let i++
+done;
+
+#fetch the inFiles and create an outDir based on their uniqueID:
+j=0
+
+echo Total files = ${#filesTotal[@]}
+
+while [ $j -lt ${#filesTotal[@]} ]; do
+	inFile1=${filesTotal[$j]}
+	inFile2=${filesTotal[$(($j+1))]}
+	uniqueID=`basename $inFile1 | sed s/_R1$inExt//`
+	outDir=$resultsDir/$uniqueID$outExt/
+			
+	mkdir -p $outDir
+
+	echo -e
+	echo This is the uniqueID:
+	echo $uniqueID
+	echo -e
+	echo This is the outDir:
+	echo $outDir
+
+	#map samples with Bowtie2:
+	bowtie_line="bowtie2 -p 16 --very-sensitive --mm -M20 --score-min=C,-15,0 -x \
+	'$genomeFile -q -1 $inFile1 -2 \
+	$inFile2' -S $outDir/$uniqueID/$uniqueID.sam"
+
+	echo -e
+	echo This is the bowtie_line:
+	echo $bowtie_line
+
+	#qsub -N bw2$uniqueID -wd $logDir -b y -j y -P GenomeInformatics -pe smp \
+	#$big_numcores -V bowtie_line
+
+	samtools_line="samtools view -hbuS $outDir/$uniqueID/$uniqueID.sam | samtools sort - \
+	'$outDir/$uniqueID/$uniqueID'"
+
+	echo -e
+	echo This is the samtools_line:
+	echo $samtools_line
+
+	#qsub -N st$uniqueID -hold_jid bw2$uniqueID -wd $logDir -b y -j y -P GenomeInformatics \
+	#-pe smp $small_numcores -V samtools_line
+
+	#extract the unmapped reads:
+	samtools_view_line="samtools view -hf 4 '$outDir/$uniqueID/$uniqueID.bam' \
+	| samtools view -Sb - > '$outDir/$uniqueID/unmapped_$uniqueID.bam'"
+
+	echo -e
+	echo This is the samtools_view_line:
+	echo $samtools_view_line
+
+	#qsub -N view$uniqueID -hold_jid st$uniqueID -wd $logDir -b y -j y -P GenomeInformatics \
+	#-pe smp $small_numcores -V samtools_view_line
+
+	#split unmapped reads onto anchors for independent mapping:
+	split_line="python $circDir/unmapped2anchors.py $outDir/$uniqueID/unmapped_$uniqueID.bam \
+	| gzip > $outDir/$uniqueID/$uniqueID_anchors.qfa.gz"
+	
+	echo -e
+	echo This is the split_line:
+	echo $split_line
+
+	#qsub -N spl$uniqueID -hold_jid stv$uniqueID -wd $logDir -b y -j y -P GenomeInformatics \
+	#-pe smp $small_numcores -V split_line
+
+	#screen for spliced reads:
+	screen_line="bowtie2 -p 16 --reorder --mm -M20 --score-min=C,-15,0 -q -x $genomeFile \
+	-U $outDir/$uniqueID/$uniqueID_anchors.qfa.gz -S $outDir/$uniqueID/$uniqueID_anchors.sam"
+
+	echo -e
+	echo This is the screen_line:
+	echo $screen_line
+
+	#qsub -N scr$uniqueID -hold_jid spl$uniqueID -wd $logDir -b y -j y -P GenomeInformatics \
+	#-pe smp $big_numcores -V screen_line
+
+	cat_line="cat $outDir/$uniqueID/$uniqueID_anchors.sam | python $circDir/find_circ.py -G \
+	$GENOME -p $uniqueID -s $outDir/$uniqueID/sites.log"
+
+	echo -e
+	echo This is the cat_line:
+	echo $cat_line
+
+	#qsub -N cat$uniqueID -hold_jid scr$uniqueID -wd $logDir -b y -j y -P GenomeInformatics -pe \
+	#smp $small_numcores -V cat_line
+
+	#filter the output:
+	grep_line="grep circ '$outDir/$uniqueID/sites.bed'| grep -v chrM | python \
+	$circDir/sum.py -2,3 | python $circDir/scorethresh.py -16 1 | python \
+	$circDir/scorethresh.py -15 2 | python $circDir/scorethresh.py -14 2 | python \		$circDir/scorethresh.py 7 2 | python $circDir/scorethresh.py 8,9 35 | python \
+	$circDir/scorethresh.py -17 100000 > $outDir/$uniqueID/circ_candidates.bed"
+
+	echo -e
+	echo This is the grep_line:
+	echo $grep_line
+
+	#qsub -N grep$uniqueID -hold_jid cat$uniqueID -wd $logDir -b y -j y -P GenomeInformatics -pe \
+	#smp 1 -V grep_line
+	j=$(($j+2))
+done;
+
+###what does following mean?###
+#to get a reasonable set of circRNA candidates try:;done
+
